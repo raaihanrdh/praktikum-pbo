47c2c66c277614078560ed33067aa0d08175cc12
raaihanrdh <100418583+raaihanrdh@users.noreply.github.com>
2023-03-22 22:53:29 +0700

    Update BujurSangkar.java

--- praktikum 4/org/bangundatar/BujurSangkar.java
+++ praktikum 4/org/bangundatar/BujurSangkar.java
@@ -1,7 +1,6 @@
 /**
 * BujurSangkar.java 20/03/2023
-* Nama      : Derva Anargya Ghaly
-* NIM       : 24060121140149
+* Nama      : Muhammad Raihan Ridho
 * Deskripsi : representasi dasar dari objek bujur sangkar
 */
 
