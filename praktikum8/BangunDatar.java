ef9c3fa769af63735d07dd017a8d9b97713acf7e
raaihanrdh <100418583+raaihanrdh@users.noreply.github.com>
2023-05-24 23:45:29 +0700

    Delete praktikum8 directory

--- praktikum8/BangunDatar.java
+++
@@ -1,9 +0,0 @@
-/**
- * File         : BangunDatar.java
- * Nama         : Muhammad Raihan Ridho
- * NIM          : 24060121140131
- * Deskripsi    : program penggunaan objek ArrayList sebagai Collection class
-*/ 
-public abstract class BangunDatar {
-    public abstract double hitungKeliling(); 
-}
